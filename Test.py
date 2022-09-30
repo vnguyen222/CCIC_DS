b7c9be331c78729bb9e89d1c37f4f2aff7390c9d
vincent <vnguyen2@cherrycreekschools.org>
2022-09-30 10:17:12 -0600

    Reorganization

--- Test.py
+++ Test.py
@@ -1,17 +1,19 @@
-# def get_missing_letters(str):
-#     missing_str = ""
+'''def get_missing_letters(str):
+    missing_str = ""
 
-#     for x in range(97, 123):
-#         if chr(x) not in str:
-#             missing_str += chr(x)
+    for x in range(97, 123):
+        if chr(x) not in str:
+            missing_str += chr(x)
     
-#     return missing_str
+    return missing_str
 
-# print(get_missing_letters("abcdefgpqrstuvwxyz"))
-# print(get_missing_letters("zyxwvutsrq"))
-# print(get_missing_letters("abc"))
-# print(get_missing_letters("abcdefghijklmnopqrstuvwxyz"))
+print(get_missing_letters("abcdefgpqrstuvwxyz"))
+print(get_missing_letters("zyxwvutsrq"))
+print(get_missing_letters("abc"))
+print(get_missing_letters("abcdefghijklmnopqrstuvwxyz"))'''
 
+
+'''
 binary = [1,0,0]
 
 def convert(bin):
@@ -24,4 +26,20 @@ def convert(bin):
     
     return dec
 
-print(convert(binary))
+print(convert(binary))'''
+
+from math import sqrt
+
+def solutions(li):
+    try:
+        x1 = ((-1*li[1]) + sqrt((li[1]**2)-(4*li[0]*li[2]))) / (2 * li[0])
+        x2 = ((-1*li[1]) - sqrt((li[1]**2)-(4*li[0]*li[2]))) / (2 * li[0])
+    except ValueError:
+        return 0
+
+    if x1 == x2:
+        return 1
+    else:
+        return 2
+
+solutions([1,0,1])
